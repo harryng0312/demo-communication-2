a6c3e5de68173e0352e5b62e91c22b67a3fd727b
harrynguyen0312 <Hiepxie1987>
2022-02-14 11:23:13 +0700

    - kafka client

---
+++ demo-communication-web/src/test/java/org/harryng/communication/kafka/TestKafka.java
@@ -0,0 +1,15 @@
+package org.harryng.communication.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestKafka {
+
+    public static Logger logger = LoggerFactory.getLogger(TestKafka.class);
+
+    @Test
+    public void testProducer(){
+        logger.info("test");
+    }
+}
