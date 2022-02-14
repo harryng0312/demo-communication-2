7ba92dfafa8f2ebd416a560e87701bb1838ce4e2
harrynguyen0312 <Hiepxie1987>
2022-02-14 15:20:36 +0700

    - kafka client

--- demo-communication-web/src/test/java/org/harryng/communication/kafka/TestKafka.java
+++ demo-communication-web/src/test/java/org/harryng/communication/kafka/TestKafka.java
@@ -1,15 +1,120 @@
 package org.harryng.communication.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.state.KeyValueStore;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class TestKafka {
 
     public static Logger logger = LoggerFactory.getLogger(TestKafka.class);
 
+    public static String APP_ID_CFG = "wordcount-app";
+    public static String GROUP_ID_CFG = "group-id";
+    public static String BOOTSTRAP_SERVERS_CFG = "localhost:9092";
+
+    public static String TOPIC_NAME = "quickstart-events";
+    public static Integer PARTITION_NO = 1;
+
     @Test
-    public void testProducer(){
-        logger.info("test");
+    public void testProducer() {
+        logger.info("test producer");
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_CFG);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "trans-1");
+
+        String testVal = "time: " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+        producer.initTransactions();
+
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>(TOPIC_NAME, testVal);
+
+        producer.beginTransaction();
+        Future<RecordMetadata> result = producer.send(record);
+        try {
+            result.get();
+            producer.flush();
+            producer.commitTransaction();
+        } catch (InterruptedException | ExecutionException e) {
+            producer.abortTransaction();
+            logger.error("", e);
+        } finally {
+            producer.close();
+        }
+    }
+
+    @Test
+    public void testConsumer() {
+        logger.info("test producer");
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_CFG);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID_CFG);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        props.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe(Arrays.asList(TOPIC_NAME));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("Key: " + record.key() + ", Value:" + record.value());
+                logger.info("Partition:" + record.partition() + ", Offset:" + record.offset());
+            }
+        }
+    }
+
+    @Test
+    public void testStream() {
+        logger.info("test stream");
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, APP_ID_CFG);
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_CFG);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> textLines = builder.stream(TOPIC_NAME);
+        KTable<String, Long> wordCounts = textLines
+                .flatMapValues(textLine -> Arrays.asList(textLine.toLowerCase().split("\\W+")))
+                .groupBy((key, word) -> word)
+                .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("counts-store"));
+        wordCounts.toStream().to("WordsWithCountsTopic", Produced.with(Serdes.String(), Serdes.Long()));
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.start();
+        logger.info("start stream...");
     }
 }
